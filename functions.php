e02bb891e96da2da3e42ef88d66a5378bab2c0bd
Julian1729 <hernandez.julian17@gmail.com>
2019-10-03 15:28:42 -0400

    Move scripts to seperate file

--- functions.php
+++ functions.php
@@ -14,24 +14,6 @@ define('JH_VENDOR', JH_THEME_PATH . '/vendor');
 define('JH_FUNCTIONS', JH_THEME_PATH . '/functions');
 
 /**
- * Enqueue Scripts
- */
-function jh_enqueue_scripts(){
-
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
-
-  wp_enqueue_script('landing_page', get_template_directory_uri() . '/assets/js/dist/landing.bundle.js', null, null, true);
-
-  wp_enqueue_style('julianhernandez', get_template_directory_uri() . '/assets/css/dist/main.css');
-
-  // Enqueue google fonts
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Oswald:300,400,700&display=swap', array('main') );
-
-}
-
-add_action('wp_enqueue_scripts', 'jh_enqueue_scripts');
-
-/**
  * Theme Support
  */
 // Add Thumbnail Support
@@ -40,6 +22,7 @@ add_theme_support('post-thumbnails');
 /**
  * Functions
  */
+require(JH_FUNCTIONS . '/enqueue-scripts.php');
 require(JH_FUNCTIONS . '/custom-post-types.php');
 require(JH_FUNCTIONS . '/shortcodes.php');
 
