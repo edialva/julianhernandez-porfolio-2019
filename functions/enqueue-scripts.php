3bf7b861b937b1ed648175273fb10b87bdb52770
Julian1729 <hernandez.julian17@gmail.com>
2019-10-03 15:28:52 -0400

    Move scripts to seperate file

---
+++ functions/enqueue-scripts.php
@@ -0,0 +1,16 @@
+<?php
+
+function jh_enqueue_scripts(){
+
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+  wp_enqueue_script('landing_page', get_template_directory_uri() . '/assets/js/dist/landing.bundle.js', null, null, true);
+
+  wp_enqueue_style('julianhernandez', get_template_directory_uri() . '/assets/css/dist/main.css');
+
+  // Enqueue google fonts
+  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Oswald:300,400,700&display=swap', array('main') );
+
+}
+
+add_action('wp_enqueue_scripts', 'jh_enqueue_scripts');
