2130ebc904e116ba016e6486f92da1e90306adae
Julian1729 <hernandez.julian17@gmail.com>
2019-10-05 14:30:19 -0400

    Use local font files

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -9,7 +9,7 @@ function jh_enqueue_scripts(){
   wp_enqueue_style('julianhernandez', get_template_directory_uri() . '/assets/css/dist/main.css');
 
   // Enqueue google fonts
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Oswald:300,400,700&display=swap', array('main') );
+  // wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Oswald:300,400,700&display=swap', array('main') );
 
 }
 
