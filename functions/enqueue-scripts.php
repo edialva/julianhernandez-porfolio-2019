e8265c9f5f16b9dd06d0f744fe3625c5f214531d
Julian1729 <hernandez.julian17@gmail.com>
2019-10-06 01:08:45 -0400

    Conditionally load js

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -4,15 +4,14 @@ function jh_enqueue_scripts(){
 
   wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-  wp_enqueue_script('landing_page', get_template_directory_uri() . '/assets/js/dist/landing.bundle.js', null, null, true);
-
   wp_enqueue_style('julianhernandez', get_template_directory_uri() . '/assets/css/dist/main.css');
-
-  // Enqueue google fonts
-  // wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Oswald:300,400,700&display=swap', array('main') );
 
   if(is_singular('portfolio')){
     wp_enqueue_script('single_portfolio', get_template_directory_uri() . '/assets/js/dist/single-portfolio.bundle.js', null, null, true);
+  }
+
+  if(is_front_page()){
+    wp_enqueue_script('landing_page', get_template_directory_uri() . '/assets/js/dist/landing.bundle.js', null, null, true);
   }
 
 }
