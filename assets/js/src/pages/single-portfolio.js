68b6139b836e062e0fc8e98364b4c99798c5737b
Julian1729 <hernandez.julian17@gmail.com>
2019-10-05 23:25:01 -0400

    Further animate single portfolio

--- assets/js/src/pages/single-portfolio.js
+++ assets/js/src/pages/single-portfolio.js
@@ -6,6 +6,7 @@ const DOM_CACHE = {
   $page_heading: $('header .heading'),
   $content_heading: $('article h1'),
   $meta: $('article .meta'),
+  $techs: $('.meta ul.tech li'),
   $the_content: $('article .the-content'),
 };
 
@@ -22,5 +23,19 @@ DOM_CACHE.$meta
   .velocity({opacity: 1, scale: 1.2}, {delay: 1300, duration: 300})
   .velocity({scale: 1});
 
+let techDuration = 200;
+let techDelay = 0;
+setTimeout(() => {
+
+  DOM_CACHE.$techs.each(function(){
+    let $this = $(this);
+    $this
+      .velocity({opacity: 1, scale: 1.2}, {delay: techDelay, duration: techDuration})
+      .velocity({scale: 1});
+    techDelay = techDelay + 50;
+  });
+
+}, 1800)
+
 DOM_CACHE.$the_content
-  .velocity({opacity: 1}, {delay: 1600, duration: 300});
+  .velocity({opacity: 1}, {delay: 2000, duration: 500});
