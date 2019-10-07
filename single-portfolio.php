ed78c20d67d3a54579904d4a0e9eedd919e6cdee
Julian1729 <hernandez.julian17@gmail.com>
2019-10-06 23:06:18 -0400

    Conditionally display post meta

--- single-portfolio.php
+++ single-portfolio.php
@@ -7,10 +7,9 @@ global $post;
 $data['custom'] = jh_post_meta();
 
 // get project technologies
-$data['technologies'] = get_terms([
-  'taxonomy' => 'technologies',
-  'fields' => 'names'
-]);
+$data['technologies'] = array_map(function($term){
+  return $term->name;
+}, wp_get_post_terms($post->ID, 'technologies', $args));
 
 get_header();
 
